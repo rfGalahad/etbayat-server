7c903125e7cdeec65b27732789ccb65acc282bf5
Ruther Solloso <rfsolloso03@gmail.com>
2026-01-28 11:27:22 +0800

    Get Duplicate

--- controllers/surveyControllers/getAllDuplicates.js
+++ controllers/surveyControllers/getAllDuplicates.js
@@ -3,41 +3,24 @@ import pool from '../../config/db.js';
 export const getAllDuplicates = async (req, res) => {
   try {
     const [rows] = await pool.query(`
-      SELECT
-        TRIM(LOWER(p.first_name)) AS match_first_name,
-        TRIM(LOWER(p.last_name)) AS match_last_name,
-        DATE_FORMAT(p.birthdate, '%m-%d-%Y') AS birthdate,
-        p.sex,
-
-        COUNT(DISTINCT h.barangay) AS barangay_count,
-
-        JSON_ARRAYAGG(
-            JSON_OBJECT(
-                'resident_id', p.resident_id,
-                'first_name', p.first_name,
-                'middle_name', p.middle_name,
-                'last_name', p.last_name,
-                'suffix', p.suffix,
-                'birthdate', CAST(DATE_FORMAT(p.birthdate, '%m-%d-%Y') AS CHAR),
-                'sex', p.sex,
-                'barangay', h.barangay,
-                'street', h.street,
-                'municipality', h.municipality,
-                'family_id', p.family_id
-            )
-        ) AS residents
-    FROM population p
-    JOIN family_information f ON f.family_id = p.family_id
-    JOIN households h ON f.household_id = h.household_id
-    GROUP BY
-        match_first_name,
-        match_last_name,
-        p.birthdate,
-        p.sex
-    HAVING COUNT(DISTINCT h.barangay) > 1
-    ORDER BY
-        match_last_name,
-        match_first_name;
+      SELECT p.*
+      FROM population p
+      JOIN (
+          SELECT
+              TRIM(LOWER(first_name)) AS first_name,
+              TRIM(LOWER(last_name)) AS last_name,
+              birthdate
+          FROM population
+          GROUP BY
+              TRIM(LOWER(first_name)),
+              TRIM(LOWER(last_name)),
+              birthdate
+          HAVING COUNT(*) > 1
+      ) d
+      ON TRIM(LOWER(p.first_name)) = d.first_name
+      AND TRIM(LOWER(p.last_name)) = d.last_name
+      AND p.birthdate = d.birthdate
+      ORDER BY p.last_name, p.first_name;
     `);
 
     res.status(200).json({
