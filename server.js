60610edd2544bb4fd66fa4799e2bcfc8acca086f
Ruther Solloso <rfsolloso03@gmail.com>
2026-02-27 10:22:53 +0800

    CORS

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 3000;
 // MIDDLEWARE
 app.use(cors({
   origin: [
-    'https://e-tbayatwmswdo.com',
+    'https://e-tbayatmswdo.com',
     'http://localhost:5173',
     'http://192.168.0.188:5173'
   ],
