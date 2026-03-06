a06cad999059fb91b29f4a1c901f89060358f13f
Ruther Solloso <rfsolloso03@gmail.com>
2026-03-06 10:36:56 +0800

    Resident Info

--- controllers/databankControllers/getResidentInfoById.js
+++ controllers/databankControllers/getResidentInfoById.js
@@ -6,94 +6,98 @@ export const getResidentInfoById = async (req, res) => {
 
     const [rows] = await pool.query(`
       SELECT
-        p.resident_id as residentId,
-        p.family_id as familyId,
+
+        f.survey_id AS surveyId,
+
+        p.resident_id AS residentId,
+        p.family_id AS familyId,
 
         CONCAT(p.first_name, ' ', 
         IFNULL(CONCAT(SUBSTRING(p.middle_name, 1, 1), '. '), ''), 
         p.last_name, ' ', 
-        IFNULL(p.suffix, '')) as fullName,
+        IFNULL(p.suffix, '')) AS fullName,
 
         p.sex,
-        DATE_FORMAT(p.birthdate, '%m-%d-%Y') as birthdate,
-        p.civil_status as civilStatus,
+        DATE_FORMAT(p.birthdate, '%m-%d-%Y') AS birthdate,
+        p.civil_status AS civilStatus,
         p.religion,
-        p.relation_to_family_head as relationToFamilyHead,
+        p.relation_to_family_head AS relationToFamilyHead,
         p.birthplace, 
 
-        pi.educational_attainment as educationalAttainment, 
+        pi.educational_attainment AS educationalAttainment, 
         pi.skills,
         pi.occupation,
         pi.company,
         pi.other_occupation AS otherOccupation,
-        pi.employment_status as employmentStatus,
-        pi.employment_category as employmentCategory,
-        pi.employment_type as employmentType,
-        pi.monthly_income as monthlyIncome,
-        pi.annual_income as annualIncome,
+        pi.employment_status AS employmentStatus,
+        pi.employment_category AS employmentCategory,
+        pi.employment_type AS employmentType,
+        pi.monthly_income AS monthlyIncome,
+        pi.annual_income AS annualIncome,
 
-        ci.contact_number as contactNumber,
-        ci.telephone_number as telephoneNumber,
-        ci.email_address as emailAddress,
+        ci.contact_number AS contactNumber,
+        ci.telephone_number AS telephoneNumber,
+        ci.email_address AS emailAddress,
 
-        hi.blood_type as bloodType,
-        hi.health_status as healthStatus,
-        hi.disability_type as disabilityType,
-        hi.disability_cause as disabilityCause,
-        hi.disability_specific as disabilitySpecific, 
+        hi.blood_type AS bloodType,
+        hi.health_status AS healthStatus,
+        hi.disability_type AS disabilityType,
+        hi.disability_cause AS disabilityCause,
+        hi.disability_specific AS disabilitySpecific, 
 
-        gi.sss as sssNumber,
-        gi.gsis as gsisNumber,
-        gi.pagibig as pagibigNumber,
-        gi.psn as psnNumber,
-        gi.philhealth as philhealthNumber,
-        gi.philsys as philsysNumber,
+        gi.sss AS sssNumber,
+        gi.gsis AS gsisNumber,
+        gi.pagibig AS pagibigNumber,
+        gi.psn AS psnNumber,
+        gi.philhealth AS philhealthNumber,
+        gi.philsys AS philsysNumber,
 
-        DATE_FORMAT(a.date_become_officer, '%m-%d-%Y') as dateBecomeOfficer,
-        DATE_FORMAT(a.date_become_member, '%m-%d-%Y') as dateBecomeMember,
-        a.organization_name as organizationName,
+        DATE_FORMAT(a.date_become_officer, '%m-%d-%Y') AS dateBecomeOfficer,
+        DATE_FORMAT(a.date_become_member, '%m-%d-%Y') AS dateBecomeMember,
+        a.organization_name AS organizationName,
 
         -- Check if affiliation exists
         CASE 
           WHEN a.resident_id IS NOT NULL 
           THEN TRUE 
           ELSE FALSE 
-        END as affiliated,
+        END AS affiliated,
 
-        ni.settlement_details as settlementDetails,
+        ni.settlement_details AS settlementDetails,
         ni.ethnicity,
-        ni.place_of_origin as placeOfOrigin,
-        ni.house_owner as houseOwner,
+        ni.place_of_origin AS placeOfOrigin,
+        ni.house_owner AS houseOwner,
 
         -- Convert transient to boolean
         CASE 
           WHEN ni.transient = 1 
           THEN TRUE 
           ELSE FALSE 
-        END as transient,
+        END AS transient,
 
         -- Check if non_ivatan exists
         CASE 
           WHEN ni.resident_id IS NOT NULL 
           THEN TRUE 
           ELSE FALSE 
-        END as nonIvatan,
+        END AS nonIvatan,
 
         CASE
           WHEN ni.date_registered IS NOT NULL
           THEN 'YES'
           ELSE 'NO'
-        END as isRegistered,
+        END AS isRegistered,
 
-        DATE_FORMAT(ni.date_registered, '%m-%d-%Y') as dateRegistered
+        DATE_FORMAT(ni.date_registered, '%m-%d-%Y') AS dateRegistered
 
       FROM population p
       LEFT JOIN professional_information pi ON p.resident_id = pi.resident_id
-      LEFT JOIN contact_information ci ON p.resident_id = ci.resident_id
-      LEFT JOIN health_information hi ON p.resident_id = hi.resident_id
-      LEFT JOIN government_ids gi ON p.resident_id = gi.resident_id
-      LEFT JOIN affiliation a ON p.resident_id = a.resident_id
-      LEFT JOIN non_ivatan ni ON p.resident_id = ni.resident_id
+      LEFT JOIN contact_information ci      ON p.resident_id = ci.resident_id
+      LEFT JOIN health_information hi       ON p.resident_id = hi.resident_id
+      LEFT JOIN government_ids gi           ON p.resident_id = gi.resident_id
+      LEFT JOIN affiliation a               ON p.resident_id = a.resident_id
+      LEFT JOIN non_ivatan ni               ON p.resident_id = ni.resident_id
+      LEFT JOIN family_information f        ON f.family_id   = p.family_id
       WHERE p.resident_id = ?`, 
       [residentId]
     );
@@ -132,8 +136,6 @@ export const getResidentInfoById = async (req, res) => {
       ...rows[0],
       ...classificationData.reduce((acc, curr) => ({ ...acc, ...curr }), {})
     };
-
-    console.log('Resident Data:', residentData);
 
     res.status(200).json({
       success: true,
