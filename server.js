7ac3174ad140e7848e8831227d1105848fc85e12
Ruther Solloso <rfsolloso03@gmail.com>
2026-04-08 06:52:11 +0800

    server fix

--- server.js
+++ server.js
@@ -38,8 +38,6 @@ app.use(cors({
   // credentials: true
 }));
 
-app.options('*', cors());
-
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
