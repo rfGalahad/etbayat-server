e1ac91308c0f91e379abdea5f32263480c6b2df4
Ruther Solloso <rfsolloso03@gmail.com>
2026-08-19 09:56:31 +0800

    new update solo parent excel

--- controllers/databankControllers/getSoloParent.js
+++ controllers/databankControllers/getSoloParent.js
@@ -7,32 +7,38 @@ export const getSoloParent = async (req, res) => {
         r.resident_id AS residentId,
         fam.family_id AS familyId,
 
-        /* Parent full name (same for all rows in the family) */
+        /* Parent full name */
         CONCAT_WS(' ',
             parent.first_name,
             parent.middle_name,
             parent.last_name,
             parent.suffix
         ) AS parentName,
+        DATE_FORMAT(parent.birthdate, '%m-%d-%Y') AS parentBirthdate,
+        TIMESTAMPDIFF(YEAR, parent.birthdate, CURDATE()) AS parentAge,
+        parent.sex AS parentSex,
+        pi_parent.educational_attainment AS parentEducationalAttainment,
+        CASE
+            WHEN pi_parent.occupation = 'Others'
+                THEN pi_parent.other_occupation
+            ELSE pi_parent.occupation
+        END AS parentOccupation,
 
-        /* Child name */
+        /* Child name and details */
         CONCAT_WS(' ',
             r.first_name,
             r.middle_name,
             r.last_name,
             r.suffix
         ) AS childName,
-
         DATE_FORMAT(r.birthdate, '%m-%d-%Y') AS birthdate,
         TIMESTAMPDIFF(YEAR, r.birthdate, CURDATE()) AS age,
         r.sex,
-
-        pi.educational_attainment AS educationalAttainment,
-
+        pi_child.educational_attainment AS educationalAttainment,
         CASE
-            WHEN pi.occupation = 'Others'
-                THEN pi.other_occupation
-            ELSE pi.occupation
+            WHEN pi_child.occupation = 'Others'
+                THEN pi_child.other_occupation
+            ELSE pi_child.occupation
         END AS occupation,
 
         sp.solo_parent_id AS soloParentId,
@@ -41,17 +47,14 @@ export const getSoloParent = async (req, res) => {
 
     FROM family_information fam
 
-    /* Get the FAMILY HEAD (parent) */
     JOIN population parent
         ON parent.family_id = fam.family_id
     AND parent.relation_to_family_head = 'Family Head'
 
-    /* Confirm the family is a solo-parent family via the parent's classification */
     JOIN social_classification sc
         ON sc.resident_id = parent.resident_id
     AND sc.classification_code = 'SP'
 
-    /* Bring in only the children (non-head residents under 22) */
     JOIN population r
         ON r.family_id = fam.family_id
     AND r.relation_to_family_head <> 'Family Head'
@@ -60,8 +63,11 @@ export const getSoloParent = async (req, res) => {
     JOIN households h
         ON h.household_id = fam.household_id
 
-    LEFT JOIN professional_information pi
-        ON pi.resident_id = r.resident_id
+    LEFT JOIN professional_information pi_child
+        ON pi_child.resident_id = r.resident_id
+
+    LEFT JOIN professional_information pi_parent
+        ON pi_parent.resident_id = parent.resident_id
 
     LEFT JOIN solo_parent_id_applications sp
         ON sp.resident_id = parent.resident_id
