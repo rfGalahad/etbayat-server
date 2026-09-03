b41cf3bffa2a1b2ff7d5feb6cb97a7ce6ec49d51
Ruther Solloso <rfsolloso03@gmail.com>
2026-09-03 20:22:31 +0800

    updated issues and concern

--- controllers/databankControllers/issuesConcern.js
+++ controllers/databankControllers/issuesConcern.js
@@ -4,13 +4,15 @@ export const getIssuesConcern = async (req, res) => {
   try {
     const [rows] = await pool.query(`
       SELECT 
-        s.survey_id,
-        h.barangay,
-        ci.community_issue AS issues
+          s.survey_id AS surveyId,
+          h.barangay,
+          ci.community_issue AS issues
       FROM surveys s
       JOIN family_information fi ON fi.survey_id = s.survey_id
       JOIN households h ON h.household_id = fi.household_id
-      LEFT JOIN community_issues ci ON ci.survey_id = s.survey_id
+      JOIN community_issues ci ON ci.survey_id = s.survey_id
+      WHERE ci.community_issue IS NOT NULL 
+        AND TRIM(ci.community_issue) <> ''
       ORDER BY s.survey_id;
     `);
     
